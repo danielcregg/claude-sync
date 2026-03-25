7ca591776c8a2ad473a26b5447276215c27c5a87
Daniel Cregg <daniel.cregg@atu.ie>
2026-03-25 18:18:23 +0000

    Remove test files, add to gitignore

--- test_error.js
+++
@@ -1,8 +0,0 @@
-﻿const {execSync} = require('child_process');
-try {
-  execSync(process.execPath + ' -e "console.error(\'ERR\'); process.exit(1)"', {encoding: 'utf8', stdio: ['pipe','pipe','pipe']});
-} catch (e) {
-  console.log('typeof stderr:', typeof e.stderr);
-  console.log('stderr instanceof Buffer:', e.stderr instanceof Buffer);
-  console.log('stderr:', JSON.stringify(e.stderr));
-}
